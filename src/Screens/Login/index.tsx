e31f64ecbf0780f8768c403630e228e7fa5457c7
Fritola <fritola23@gmail.com>
2020-08-26 23:39:53 -0300

    handle login and set to localStorage

--- src/Screens/Login/index.tsx
+++ src/Screens/Login/index.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from '../../services/api'
 
 import {LoginContainer, FormLogin, FormButton, ButtonContainer} from './styles'
 
 const Login:React.FC = () => {
+    const [login, setLogin] = useState({email: '', password: ''})
+
+    const HandleInput = (e:any) => {                
+        const {name, value} = e.target
+        setLogin({...login, [name]: value})          
+    }
+
+    const HandleLogin = async () => {
+        const response = await api.post('/login', {
+            email: login.email,
+            password: login.password
+        })
+        if(response){
+            localStorage.setItem('user', JSON.stringify(response.data.user))
+        }
+        console.log(response.data)
+        
+    }
+
     return(
         <LoginContainer>
             <FormLogin>
-                <input type="email" name="email" id="" placeholder="Email"/>
-                <input type="password" name="password" id="" placeholder="Password"/>
-                <ButtonContainer>
+                <input onChange={(e) => HandleInput(e)} type="email" value={login.email} name="email" id="" placeholder="Email"/>
+                <input onChange={(e) => HandleInput(e)} type="password" value={login.password} name="password" id="" placeholder="Password"/>
+                <ButtonContainer onClick={HandleLogin}>
                     <FormButton type="button">Enter</FormButton>
                 </ButtonContainer>
             </FormLogin>
